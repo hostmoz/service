d0bae2334fe53f191c91e2e861ced9c8b9703293
TARIQUL\spn4 <spn4@spondonit.com>
2021-07-24 11:54:30 +0600

    revoke button color issue

--- resources/views/license/revoke.blade.php
+++ resources/views/license/revoke.blade.php
@@ -27,10 +27,8 @@
 
                                 <div class="col-lg-12 text-center">
                                     <div class="mt-40 d-flex justify-content-between">
-                                        <button type="button" class="primary-btn tr-bg"
-                                                data-dismiss="modal">Cancel
-                                        </button>
-                                        <input class="primary-btn fix-gr-bg" type="submit" value="Revoke License">
+                                        <button type="button" class="primary-btn tr-bg" data-dismiss="modal">Cancel</button>
+                                        <button class="primary-btn fix-gr-bg" type="submit">Revoke License</button>
                                     </div>
                                 </div>
                             </div>
